f46c3de4f9a77b274c68e082ac211663c11399a3
PrasadPalkar <explorehorizon15@gmail.com>
2020-01-27 12:25:10 +0530

    ADDED:SingletCombination

--- flipcoin.sh
+++ flipcoin.sh
@@ -1,8 +1,18 @@
 #!/bin/bash -x
+declare -A FlipCoin
+read -p "No Of Iteration:" no
+headCount=0
+tailCount=0
+for ((i=1;i<=no;i++))
+do
 flip=$((RANDOM%2))
 if [ $flip -eq 1 ]
 then 
-head=$head+1
+FlipCoin[head]=$((++headCount))
 else
-tail=$tail+1
+FlipCoin[tail]=$((++tailCount))
 fi
+done
+percentageofhead=$((FlipCoin[head]*100/$no))
+percentageoftail=$((FlipCoin[tail]*100/$no))
+
